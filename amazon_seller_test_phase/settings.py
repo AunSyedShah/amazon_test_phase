23b7784742efd71d10143f941e4ca7caf59f511c
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-03-29 07:19:45 +0500

    to submit 2

--- amazon_seller_test_phase/settings.py
+++ amazon_seller_test_phase/settings.py
@@ -39,9 +39,6 @@ INSTALLED_APPS = [
     'main_app',
     'crispy_forms',
     'crispy_bootstrap5',
-    'api.apps.ApiConfig',
-    'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -119,9 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -131,9 +125,3 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
