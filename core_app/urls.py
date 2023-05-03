8a817c5f5996b05a5380d6803540fe07d57e493d
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-05-03 18:44:56 +0500

    revise urls

--- core_app/urls.py
+++ core_app/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('signin/', views.signin, name='signin'),
-    path('contact_us/', views.contact_us, name='contact_us'),
+    path('login/', views.signin, name='signin'),
+    path('contact/', views.contact_us, name='contact_us'),
     path('logout/', views.user_logout, name='logout'),
-    path('register/', views.register, name='register'),
+    path('signup/', views.register, name='register'),
     path('uploaded_images/', views.uploaded_images, name='uploaded_images'),
     path('uploaded_videos/', views.uploaded_videos, name='uploaded_videos'),
 ]
